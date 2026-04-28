02be1b69f388ffd74a28d6e2383f60dc1d29b305
Md Ismail Quraishi <mdismailquraishicse@gmail.com>
2026-04-28 09:53:38 +0530

    feat: define function distance_bucket

--- src/utils/basics_info.py
+++ src/utils/basics_info.py
@@ -22,9 +22,22 @@ def display_basic_info(df:pd.DataFrame):
 
 
 def extend_date(df:pd.DataFrame, colname:str):
+
     df[colname] = pd.to_datetime(df[colname])
     df["year"] = df[colname].dt.year
     df["month"] = df[colname].dt.month
     df["day"] = df[colname].dt.day
     df["week_of_year"] = df[colname].dt.isocalendar().week
+    return df
+
+
+def distance_bucket(df: pd.DataFrame):
+
+    bins = [0, 1000, 5000, 10000, float("inf")]
+    labels = ["very_close", "close", "medium", "far"]
+    df["CompetitionDistanceBucket"] = pd.cut(
+        df["CompetitionDistance"],
+        bins=bins,
+        labels=labels
+    )
     return df
